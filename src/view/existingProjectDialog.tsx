78fd6514307b5a3f5b4cfdf141a597badba3f89c
Louis-Philippe Carignan <lpcarignan@gmail.com>
2017-02-09 09:15:54 -0500

    Created existing project dialog box

--- src/view/existingProjectDialog.tsx
+++ src/view/existingProjectDialog.tsx
@@ -7,13 +7,16 @@ import * as ReactDOM from "react-dom";
 
 import { SimulationResult } from "../model/simulationResult";
 import { ResultsDisplay } from "./resultsDisplay";
+import { ExcelLoader} from "../model/io/excelLoader";
 
 import { Tabs, Tab } from "material-ui/Tabs";
 import FlatButton from "material-ui/FlatButton";
 import { Stepper, Step, StepButton, StepLabel, StepContent } from "material-ui/Stepper";
 import RaisedButton from "material-ui/RaisedButton";
 import TextField from "material-ui/TextField";
-import { cyan500, red500 } from 'material-ui/styles/colors';
+import MenuItem from "material-ui/MenuItem";
+import SelectField from "material-ui/SelectField";
+import {grey900} from 'material-ui/styles/colors';
 
 export interface ExistingProjectDialogProps {
     open: boolean;
@@ -21,10 +24,22 @@ export interface ExistingProjectDialogProps {
 }
 export interface ExistingProjectDialogState {
     stepIndex: number,
-    filename: string
+    dataFilename: string,
+    columns:Array<string>,
+    startColumn:string,
+    endColumn:string,
+    forecastsFilename: string
 }
 
 export class ExistingProjectDialog extends React.Component<ExistingProjectDialogProps, ExistingProjectDialogState>{
+
+    private static filename:string;
+
+    private excelBlob:File;
+    private excelHtmlInput:any;
+
+    private jsonBlob:File;
+    private jsonHtmlInput:any
 
     constructor(props: ExistingProjectDialogProps) {
         super(props);
@@ -33,59 +48,69 @@ export class ExistingProjectDialog extends React.Component<ExistingProjectDialog
 
     render(): JSX.Element {
 
-        const stepIndex = this.state.stepIndex;
+        // This block of code builds the 2 selectors that will show up under
+        // the Excel file upload. It will contain the columns of the spreadsheet
+        const startSelector = this.buildSelector("Start", (value:string) => {this.state.startColumn = value;}, () => {return this.state.startColumn}, this.handleChangeStartColumn);
+        const endSelector = this.buildSelector("End", (value:string) => {this.state.endColumn = value;}, () => {return this.state.endColumn}, this.handleChangeEndColumn);
 
         return <Tabs>
             <Tab label="Input">
-                <Stepper
-                    linear={false}
-                    orientation="vertical"
-                    activeStep={this.state.stepIndex}>
-                    <Step>
-                        <StepButton onTouchTap={() => this.handleBtnStep(0)}>
-                            Load data from external tool
-                        </StepButton>
-                        <StepContent>
-                            <TextField
-                                id="fileUploadTextField"
-                                value={this.state.filename}
-                                floatingLabelFocusStyle={this.styles.validStyle}
-                                floatingLabelText="Select the file to upload"
-                                hintText="Choose the file"
-                                underlineFocusStyle={this.styles.validStyle}
-                            />
-                            <RaisedButton
-                                label="Pick a file"
-                                style={this.styles.button}
-                                containerElement="label"
-                            >
-                                <input
-                                    type="file"
-                                    onChange={this.handleFileChange.bind(this)}
-                                    style={this.styles.exampleImageInput}
-                                />
-                            </RaisedButton>
-                            {this.renderStepActions(0)}
-                        </StepContent>
-                    </Step>
-                    <Step>
-                        <StepButton onTouchTap={() => this.handleBtnStep(1)}>
-                            Load previous forecasts (optional)
-                        </StepButton>
-                        <StepContent>
-                            {this.renderStepActions(1)}
-                        </StepContent>
-                    </Step>
-                    <Step>
-                        <StepButton onTouchTap={() => this.handleBtnStep(2)}>
-                            Confirm forecasts generation
-                        </StepButton>
-                        <StepContent>
-
-                            {this.renderStepActions(2)}
-                        </StepContent>
-                    </Step>
-                </Stepper>
+                <div>Load data from external tool</div>
+                <div>
+                    <RaisedButton
+                        label="Select an Excel file"
+                        style={this.styles.button}
+                        containerElement="label"
+                    >
+                        <input
+                            type="file"
+                            onChange={this.handleExcelFileChange.bind(this)}
+                            style={this.styles.input}
+                        />
+                    </RaisedButton>
+                    <TextField
+                        id="fileDataUploadTextField"
+                        value={this.state.dataFilename}
+                        floatingLabelText="Excel file to upload"
+                        disabled={true}
+                        floatingLabelStyle={this.styles.textField}
+                        inputStyle={this.styles.textField}
+                        underlineStyle={this.styles.textField}
+                    />             
+                    <div>               
+                        {startSelector}
+                        {endSelector}
+                    </div>                
+                </div>
+                <div>Load previous forecasts (optional)<div>
+                    <RaisedButton
+                        label="Select a .json file"
+                        style={this.styles.button}
+                        containerElement="label"
+                    >
+                        <input
+                            type="file"
+                            onChange={this.handleJsonFileChange.bind(this)}
+                            style={this.styles.input}
+                        />
+                    </RaisedButton>
+                    <TextField
+                        id="fileForecastsTextField"
+                        value={this.state.forecastsFilename}
+                        floatingLabelText="Previous forecasts to upload"
+                        disabled={true}
+                        floatingLabelStyle={this.styles.textField}
+                        inputStyle={this.styles.textField}
+                        underlineStyle={this.styles.textField}
+                    />             
+                    </div>
+                </div>
+                <div>
+                    <RaisedButton
+                        label="Create forecasts"
+                        style={this.styles.button}
+                        containerElement="label"/>                    
+                </div>
             </Tab>
             <Tab label="Forecasts">
                 <ResultsDisplay
@@ -96,101 +121,139 @@ export class ExistingProjectDialog extends React.Component<ExistingProjectDialog
         </Tabs>;
     }
 
-    private handleBtnStep(index: number): void {
-        this.state.stepIndex = index;
-        this.setState(this.state);
-    }
+    private handleExcelFileChange(eventInput: any): void {
+        this.excelBlob = eventInput.target.files[0];
+        this.excelHtmlInput = eventInput.target;
 
-    private handleFileChange(e: any): void {
-        let file = e.target.files[0];
+        this.state.startColumn = this.state.endColumn = "";
+        this.state.dataFilename = this.excelBlob.name;
+        ExistingProjectDialog.filename = this.excelBlob.name;        
+        
         let fileReader = new FileReader();
-        fileReader.onload = function (e: any) {
-            var data = e.target.result;
+        fileReader.onload = this.loadInternal;
+        fileReader.onloadstart = this.loadStart;
+        fileReader.onerror = this.loadError;        
 
-            var workbook;
-            // if(rABS) {
-            //     /* if binary string, read with type 'binary' */
-            //     workbook = XLSX.read(data, {type: 'binary'});
-            // } else {
-            //     /* if array buffer, convert to base64 */
-            //     var arr = fixdata(data);
-            //     workbook = XLSX.read(btoa(arr), {type: 'base64'});
-            // }
-
-            /* DO SOMETHING WITH workbook HERE */
-        };
-        fileReader.onloadend = function (e: any) {
-            console.log("File load " + e);
+        fileReader.onloadend = (eventFileReader:any) => {
+            console.log(`File ${ExistingProjectDialog.filename} loaded successfully with ${eventFileReader.loaded} bytes`);
+            this.callbackExcelFileChange(eventInput, eventFileReader.target.columns);
         }
-        fileReader.readAsBinaryString(file);
-
-        this.state.filename = file.name;
-        this.setState(this.state);
-    }
-
-    private onFileLoad(event: any): void {
-
-    }
-
-    private handleBtnBack(event: any): void {
-        let stepIndex = this.state.stepIndex;
-        if (stepIndex > 0)
-            this.state.stepIndex = -1;
+        fileReader.readAsBinaryString(this.excelBlob);  
 
         this.setState(this.state);
     }
 
-    private handleBtnNext(event: any): void {
-        let stepIndex = this.state.stepIndex;
-        if (stepIndex < 2)
-            this.state.stepIndex = stepIndex + 1;
+    private callbackExcelFileChange(eventInput: any, columns:any)
+    {
+        this.excelHtmlInput.value = "";
+        this.state.columns = columns;
+        this.setState(this.state);
+    }
+
+    private handleJsonFileChange(eventInput: any): void {
+        this.jsonBlob = eventInput.target.files[0];
+        this.jsonHtmlInput = eventInput.target;
+
+        this.state.startColumn = this.state.endColumn = "";
+        this.state.forecastsFilename = this.jsonBlob.name;
+        //ExistingProjectDialog.filename = this.jsonBlob.name;        
+        
+        let fileReader = new FileReader();
+        fileReader.onload = this.loadInternal;
+        fileReader.onloadstart = this.loadStart;
+        fileReader.onerror = this.loadError;        
+
+        fileReader.onloadend = (eventFileReader:any) => {
+            console.log(`File ${ExistingProjectDialog.filename} loaded successfully with ${eventFileReader.loaded} bytes`);
+            this.callbackJsonFileChange(eventInput, eventFileReader.target.columns);
+        }
+        fileReader.readAsBinaryString(this.excelBlob);  
 
         this.setState(this.state);
     }
 
-    private renderStepActions(step: number): JSX.Element {
-        const stepIndex = this.state.stepIndex
+    private callbackJsonFileChange(eventInput: any, columns:any)
+    {
+        this.jsonHtmlInput.value = "";
+    }
 
-        return (
-            <div style={{ margin: '12px 0' }}>
-                <RaisedButton
-                    label={stepIndex == 2 ? "Finish" : "Next"}
-                    disableTouchRipple={true}
-                    disableFocusRipple={true}
-                    primary={true}
-                    onTouchTap={this.handleBtnNext.bind(this)}
-                    style={{ marginRight: 12 }}
-                />
-                {step > 0 && (
-                    <FlatButton
-                        label="Back"
-                        disableTouchRipple={true}
-                        disableFocusRipple={true}
-                        onTouchTap={this.handleBtnBack.bind(this)}
-                    />
-                )}
-            </div>
-        );
+    private loadInternal(e:any): void{
+        let el = new ExcelLoader(ExistingProjectDialog.filename, e.target.result);
+        el.load();
+        e.target.columns = el.Columns;        
+    }
+
+    private loadStart(e:any): void{
+        console.log(`File ${ExistingProjectDialog.filename} being loaded`);
+    }
+
+    private loadError(e:any): void{
+        console.log(`File ${ExistingProjectDialog.filename} failed: Error ${e}`);
+    }
+
+    private handleChangeStartColumn(event:any, index:number, value:any): void {
+        this.state.startColumn = value;
+        this.setState(this.state);
+    }
+
+    private handleChangeEndColumn(event:any, index:number, value:any): void {
+        this.state.endColumn = value;
+        this.setState(this.state);
+    }
+
+    private buildSelector(title:String, 
+                          setStateVariable:(value:string) => void,
+                          getStateVariable:() => string, 
+                          handler:(event:any, index:number, value:any) => void): JSX.Element{        
+        
+        if (this.state.columns.length > 0){
+            
+            let value = this.state.columns[0];
+            if (getStateVariable() == "") 
+                setStateVariable(value);
+            else
+                setStateVariable(getStateVariable());
+
+            const menuItems = new Array<JSX.Element>(0);
+            for (let column of this.state.columns)
+                menuItems.push(
+                    <MenuItem 
+                        value={column} 
+                        key={title + "." + column} 
+                        primaryText={column} />     
+                );
+
+            return <SelectField
+                    floatingLabelText={title}
+                    value={getStateVariable()}
+                    onChange={handler.bind(this)}>
+                    {menuItems}
+                </SelectField>;
+        }
+        else
+            return <div>Error, could not load Excel file</div>;
     }
 
     private setInitialState(): void {
         this.state = {
             stepIndex: 0,
-            filename: ""
+            dataFilename: "",
+            columns: new Array<string>(0),
+            startColumn: "",
+            endColumn: "",
+            forecastsFilename: ""
         };
     }
 
     readonly styles = {
-        validStyle: {
-            color: cyan500
-        },
-        errorStyle: {
-            color: red500
+        textField: {
+            color: grey900,
+            cursor: 'cursor',
         },
         button: {
             margin: 12,
         },
-        exampleImageInput: {
+        input: {
             cursor: 'pointer',
             position: 'absolute',
             top: 0,
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ module.exports = {
     output: {
         filename: "bundle.js",
         path: "/dist",
-        devtoolModuleFilenameTemplate: function(info){
+        devtoolModuleFilenameTemplate: function (info) {
             return "../" + info.resourcePath;
-  }        
+        }
     },
 
     // Enable sourcemaps for debugging webpack's output.
@@ -23,7 +23,13 @@ module.exports = {
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
         ]
     },
-    
+    node: {
+        fs: 'empty'
+    },
+    externals: [
+        { './cptable': 'var cptable' },
+        { './jszip': 'jszip' }
+    ]
     // When importing a module whose path matches one of the following, just
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
