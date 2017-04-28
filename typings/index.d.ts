bf9b31b6c7b1b670de7d98113aeb07b97488a860
Louis-Philippe Carignan <lpcarignan@gmail.com>
2017-04-28 08:39:59 -0400

    Re-added xlsx (was removed by mistake)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="modules/body-parser/index.d.ts" />
 /// <reference path="modules/express-formidable/index.d.ts" />
 /// <reference path="modules/formidable/index.d.ts" />
+/// <reference path="modules/xlsx/index.d.ts" />
